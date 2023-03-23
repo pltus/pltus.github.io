51d1300ae3da06e51d420a050278526f7e7a899e
pltus <46567881+pltus@users.noreply.github.com>
2023-03-23 12:11:11 -0700

    CSS color text

--- script.js
+++ script.js
@@ -2,12 +2,18 @@ let colorIndex = 0;
 let greyscale = 0;
 const colors = ["red", "green", "blue", "white"];
 
+const colorText = document.getElementById("color-text");
+const greyscaleText = document.getElementById("greyscale-text");
+
 function updateBackgroundColor() {
   if (colorIndex === colors.length) {
     document.body.style.backgroundColor = `rgb(${greyscale}, ${greyscale}, ${greyscale})`;
+    colorText.textContent = "Color: Greyscale";
   } else {
     document.body.style.backgroundColor = colors[colorIndex];
+    colorText.textContent = `Color: ${colors[colorIndex].charAt(0).toUpperCase() + colors[colorIndex].slice(1)}`;
   }
+  greyscaleText.textContent = `Greyscale: ${greyscale}`;
 }
 
 document.addEventListener("keydown", function (event) {
@@ -16,10 +22,10 @@ document.addEventListener("keydown", function (event) {
     colorIndex = (colorIndex + colors.length - 1) % colors.length;
   } else if (key === "ArrowRight") {
     colorIndex = (colorIndex + 1) % colors.length;
-    } else if (key === "ArrowUp") {
+  } else if (key === "ArrowUp") {
     greyscale = Math.min(255, greyscale + 1);
     colorIndex = colors.length; // Set to greyscale mode
-    } else if (key === "ArrowDown") {
+  } else if (key === "ArrowDown") {
     greyscale = Math.max(0, greyscale - 1);
     colorIndex = colors.length; // Set to greyscale mode
     } else {
